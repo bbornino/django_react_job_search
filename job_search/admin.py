aa2f9d6deae805536cb4fcf3e0cb744be639b136
Brian Bornino <brianbornino@gmail.com>
2024-07-28 06:42:28 -0700

    Register Email Opportunity in Admin site

--- job_search/admin.py
+++ job_search/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EmailOpportunity
 
+class EmailOpportunityAdmin(admin.ModelAdmin):
+    list_display = ('recruiter_name', 'recruiter_company', 'job_title')
+    
 # Register your models here.
+admin.site.register(EmailOpportunity, EmailOpportunityAdmin)
