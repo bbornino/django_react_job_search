685bf23591bb7466fe9cbc30b7985a8ce3127143
Brian Bornino <brianbornino@gmail.com>
2024-07-28 06:36:46 -0700

    define EmailOpportunity model

--- job_search/models.py
+++ job_search/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class EmailOpportunity(models.Model):
+    recruiter_name = models.CharField(max_length=64)
+    recruiter_company = models.CharField(max_length=64)
+    job_title = models.CharField(max_length=128)
+    opportunity_status = models.CharField(max_length=24)
+    job_description = models.TextField()
+
+    def _str_(self):
+        return self.job_title
