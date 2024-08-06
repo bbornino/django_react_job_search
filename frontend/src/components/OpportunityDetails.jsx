608341f370f4429be1da4125bd0e9701051b6d3e
Brian Bornino <brianbornino@gmail.com>
2024-08-06 15:15:32 -0700

    Enable Loading and Saving Opportunity

--- frontend/src/components/OpportunityDetails.jsx
+++ frontend/src/components/OpportunityDetails.jsx
@@ -1,4 +1,5 @@
-import {React, Component, setState} from "react";
+import React, { Component, setState, getState} from "react";
+import {useParams , renderMatches} from "react-router-dom";
 import axios from "axios";
 import { JOB_OPPORTUNITY_API_URL } from "../constants";
 import {Form, FormGroup, Input, Label, Button, ButtonGroup, Container, Row, Col, Card, CardTitle, CardBody} from 'reactstrap';
@@ -11,7 +12,7 @@ import { faTrash, faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
 
 class OpportunityDetails extends Component {
     state = {
-        pk: 0,
+        opportunity_id: 0,
         recruiter_name: '',
         job_title: '',
         opportunity_status: '4 - No Response',
@@ -19,12 +20,30 @@ class OpportunityDetails extends Component {
         job_description: '',
     }
 
+    constructor(props) {
+        super(props)
+    }
+
+    getOpportunity = (opportunity_id) => {
+        // console.log("getOpportunity received " + opportunity_id)
+        // axios.get(JOB_OPPORTUNITY_API_URL + opportunity_id).then(res => this.setState({ opportunity: res.data }));
+        axios.get(JOB_OPPORTUNITY_API_URL + opportunity_id).then(res => {
+            this.setState({recruiter_name: res.data.recruiter_name,
+                recruiter_company: res.data.recruiter_company,
+                job_title: res.data.job_title,
+                opportunity_status: res.data.opportunity_status,
+                job_description: res.data.job_description,
+            })
+            // this.setState({ opportunity: res.data })
+        });
+    };
+    
     componentDidMount() {
-        if (this.props.opportunity) {
-            const {pk, recruiter_name, job_title, opportunity_status, 
-                recruiter_company, job_description} = this.props.opportunity;
-                this.setState({pk, recruiter_name, job_title, opportunity_status, 
-                    recruiter_company, job_description});
+        const pathArr = window.location.pathname.split('/')
+        if (pathArr[2] !== undefined) {
+            const opportunity_id = pathArr[2]
+            this.setState({opportunity_id: pathArr[2]})
+            this.getOpportunity(opportunity_id)
         }
     }
 
@@ -35,50 +54,45 @@ class OpportunityDetails extends Component {
     createOpportunity = e => {
         e.preventDefault();
         axios.post(JOB_OPPORTUNITY_API_URL, this.state).then(() => {
-            this.props.resetState();
+            window.location = '/opportunities/'
         })
     }
 
-    editOpportunity = e => {
+     editOpportunity = e => {
         e.preventDefault();
-        axios.put(JOB_OPPORTUNITY_API_URL + this.state.pk, this.state).then(() => {
-            this.props.resetState();
+        axios.put(JOB_OPPORTUNITY_API_URL + this.state.opportunity_id, this.state).then(() => {
+            window.location = '/opportunities/'
         })
     }
-
-    defaultIfEmpty = value => {
-        return value === "" ? "" : value;
-    };
 
     render() {
         return (
             <Container className="flex">
                 <Card className="my-3">
-                    <CardTitle>
-                        <Row className="my-3 mx-3">
-                            <Col md="6" sm="9" xs="6">
-                                <h3>Opportunity Details</h3>
-                            </Col>
-                            <Col md="6" sm="3" xs="6" className="pull-right">
-                                <Button color="danger" className="mx-2  pull-right" >
-                                    <FontAwesomeIcon icon={faTrash} /> &nbsp; Delete</Button>
-                                <Button color="primary" type="submit" className="mx-2 pull-right">
-                                    <FontAwesomeIcon icon={faFloppyDisk} /> &nbsp; Save</Button>
-                                
-                            </Col>
-                        </Row>
-                    </CardTitle>
+
                     <CardBody>
-                        <Form onSubmit={this.props.opportunity ? this.editOpportunity : this.createOpportunity} >
+                        <Form onSubmit={this.state.opportunity_id == 0 ? this.createOpportunity : this.editOpportunity} >
+                            <Row className="my-3 mx-3">
+                                <Col md="6" sm="9" xs="6">
+                                    <h3>Opportunity Details</h3>
+                                </Col>
+                                <Col md="6" sm="3" xs="6" className="pull-right">
+                                    <Button color="danger" className="mx-2  pull-right" >
+                                        <FontAwesomeIcon icon={faTrash} /> &nbsp; Delete</Button>
+                                    <Button color="primary" type="submit" className="mx-2 pull-right">
+                                        <FontAwesomeIcon icon={faFloppyDisk} /> &nbsp; Save</Button>
+                                    
+                                </Col>
+                            </Row>
                             <Row>
                                 <Col md="6">
                                     <FormGroup>
                                         <Label for="recruiter_name">Recruiter Name</Label>
                                         <Input
-                                            type="text"
+                                            type="text" required
                                             name="recruiter_name"
                                             onChange={this.onChange}
-                                            value={this.defaultIfEmpty(this.state.recruiter_name)}
+                                            value={this.state.recruiter_name ?? ''}
                                         />
                                     </FormGroup>
                                 </Col>
@@ -86,10 +100,10 @@ class OpportunityDetails extends Component {
                                     <FormGroup>
                                         <Label for="recruiter_company">Recruiter Company</Label>
                                         <Input
-                                            type="text"
+                                            type="text" required
                                             name="recruiter_company"
                                             onChange={this.onChange}
-                                            value={this.defaultIfEmpty(this.state.recruiter_company)}
+                                            value={this.state.recruiter_company ?? ''}
                                         />
                                     </FormGroup>
                                 </Col>
@@ -99,10 +113,10 @@ class OpportunityDetails extends Component {
                                     <FormGroup>
                                         <Label for="job_title">Job Title</Label>
                                         <Input
-                                            type="text"
+                                            type="text" required
                                             name="job_title"
                                             onChange={this.onChange}
-                                            value={this.defaultIfEmpty(this.state.job_title)}
+                                            value={this.state.job_title ?? ''}
                                         />
                                     </FormGroup>
                                 </Col>
@@ -110,10 +124,23 @@ class OpportunityDetails extends Component {
                                     <FormGroup>
                                         <Label for="opportunity_status">Status</Label>
                                         <Input
-                                            type="text"
+                                            type="text" required
                                             name="opportunity_status"
                                             onChange={this.onChange}
-                                            value={this.defaultIfEmpty(this.state.opportunity_status)}
+                                            value={this.state.opportunity_status ?? ''}
+                                        />
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md="12">
+                                    <FormGroup>
+                                        <Label for="job_description">Job Description</Label>
+                                        <Input
+                                            type="text" required
+                                            name="job_description"
+                                            onChange={this.onChange}
+                                            value={this.state.job_description ?? ''}
                                         />
                                     </FormGroup>
                                 </Col>
