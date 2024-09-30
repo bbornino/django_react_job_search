c5a9c41e27d3c521b5bb79ce23f8154ffe0ed254
Brian Bornino <brianbornino@gmail.com>
2024-09-30 13:18:21 -0700

    Remove Unused OpportunityList elements

--- frontend/src/components/OpportunityList.js
+++ frontend/src/components/OpportunityList.js
@@ -9,11 +9,6 @@ import {Button, Container, Row, Col} from 'reactstrap';
 class OpportunityList extends Component {
     state = {
         opportunities: [],
-        recruiter_name: '',
-        job_title: '',
-        opportunity_status: '4 - No Response',
-        recruiter_company: '',
-        job_description: '',
     };
 
     componentDidMount() {
@@ -46,13 +41,6 @@ class OpportunityList extends Component {
         },
     ];
 
-    submit = (e) => {
-        e.preventDefault();
-        console.log("I might submit")
-        // post(route('opportunities.store'));
-
-    };
-
     onRowClicked = (row, event) => { 
         window.location = '/opportunity-details/' + row.id
     };
@@ -61,24 +49,22 @@ class OpportunityList extends Component {
     render() {
         const { opportunities } = this.state;
         return (
-            <div>
-                <Container className="mt-2">
-                    <Row className="m-4">
-                        <Col xxl="10" xl="9" lg="9" md="8" sm="5" xs="3">
-                            <h1>All Opportunities</h1>
-                        </Col>
-                        <Col xxl="2" xl="3" lg="3" md="4" sm="7" xs="9" className="pull-right">
-                            <Link to='/opportunity-details'>
-                                <Button>
-                                    Create Opportunity
-                                </Button>
-                            </Link>
-                        </Col>
-                    </Row>
-                    <DataTableBase columns={this.columns} data={opportunities} onRowClicked={this.onRowClicked} />
-                </Container>
-            </div>
-            
+            <Container className="mt-2">
+                <Row className="m-4">
+                    <Col xxl="10" xl="9" lg="9" md="8" sm="5" xs="3">
+                        <h1>All Opportunities</h1>
+                    </Col>
+                    <Col xxl="2" xl="3" lg="3" md="4" sm="7" xs="9" className="pull-right">
+                        <Link to='/opportunity-details'>
+                            <Button>
+                                Create Opportunity
+                            </Button>
+                        </Link>
+                    </Col>
+                </Row>
+                <DataTableBase  columns={this.columns} data={opportunities} 
+                                onRowClicked={this.onRowClicked} />
+            </Container>
         )
     }
 }
