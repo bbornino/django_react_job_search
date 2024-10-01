a690359a22bf581ee6beb3ca219795cdfb859ecd
Brian Bornino <brianbornino@gmail.com>
2024-09-30 17:49:36 -0700

    JobSiteView Display properties

--- frontend/src/components/JobSiteView.jsx
+++ frontend/src/components/JobSiteView.jsx
@@ -1,8 +1,12 @@
 import React, { Component} from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 import { JOB_SITE_API_URL, formatDisplayDateTime } from "../constants";
-import {Form, FormGroup, Input, Label, Button, Container, Row, Col, Card, CardTitle, CardBody} from 'reactstrap';
+import {Button, Container, Row, Col, Card, CardTitle, CardBody} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faFloppyDisk, faPencil } from '@fortawesome/free-solid-svg-icons'
 
 class JobSiteView extends Component {
     state = {
@@ -29,10 +33,6 @@ class JobSiteView extends Component {
     getJobSite = (jobSiteId) => {
         console.log("getJobSite received " + jobSiteId);
         axios.get(JOB_SITE_API_URL + jobSiteId).then(res => {
-            debugger;
-            console.log("Processing")
-            console.log(res)
-            
             this.setState({
                 job_site_id: res.data.id,
                 site_name: res.data.site_name,
@@ -47,13 +47,69 @@ class JobSiteView extends Component {
         })
     }
 
+    onEditClicked = (r,e) => {
+        window.location = '/job-site-edit/' + this.state.job_site_id
+    }
 
     render() {
         return (
-            <Container>
+            <Container className="mt-2">
+                <Row className="m-4">
+                    <Col xxl="10" xl="9" lg="9" md="8" sm="5" xs="3">
+                        <h1>{this.state.site_name}</h1>
+                    </Col>
+                    <Col>
+                        <Button color="success" type="button"
+                                className="m-2"
+                                onClick={this.onEditClicked}>
+                            <FontAwesomeIcon icon={faPencil} /> &nbsp; Edit
+                        </Button>
+                    </Col>
+                </Row>
                 <Row>
                     <Col>
-                        Hello World today
+                        <dl>
+                            <dt>URL</dt>
+                            <dd>
+                                <Link to='{this.state.site_url}'>
+                                    {this.state.site_url}
+                                </Link>
+                            </dd>
+                        </dl>
+                    </Col>
+                    <Col>
+                        <dl>
+                            <dt>Rating</dt>
+                            <dd>{this.state.rating}</dd>
+                        </dl>
+                    </Col>
+                    <Col>
+                        <dl>
+                            <dt>Last Visited</dt>
+                            <dd>{this.state.last_visited_at}</dd>
+                        </dl>
+                    </Col>
+                    <Col>
+                        <dl>
+                            <dt>Resume Last Updated</dt>
+                            <dd>{this.state.resume_updated_at}</dd>
+                        </dl>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <dl>
+                            <dt>Headline</dt>
+                            <dd>{this.state.headline}</dd>
+                        </dl>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <dl>
+                            <dt>Description</dt>
+                            <dd>{this.state.description}</dd>
+                        </dl>
                     </Col>
                 </Row>
             </Container>
