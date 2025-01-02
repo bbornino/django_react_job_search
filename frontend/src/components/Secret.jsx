3be564e1a749718a642855bfd96dbbdcb3bdbafc
Brian Bornino <brianbornino@gmail.com>
2025-01-02 07:10:27 -0800

    Add Basic Secret Restricted Component

--- frontend/src/components/Secret.jsx
+++ frontend/src/components/Secret.jsx
@@ -1,22 +1,5 @@
-import React, { Component } from "react";
-import {Container, Row, Col} from 'reactstrap';
+import React from 'react';
 
-class Secret extends Component {
-    componentDidMount() {
-        document.title = "Release History - Job Search Tracker";
-    }
-
-    render() {
-        return (
-            <Container className="mt-4 p-t-10">
-                <Row className="text-center mb-3">
-                    <Col>
-                        <h1>SUPER SECRET PAGE!</h1>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+export default function Secret() {
+  return <div>Secret Content - Only accessible if logged in!</div>;
 }
-
-export default Secret;
