211a0c4e733d7ff517adc4cff5f2cb3edef72f3f
Brian Bornino <brianbornino@gmail.com>
2025-01-03 12:12:14 -0800

    Use React StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from 'react-auth-kit';
@@ -10,7 +10,9 @@ root.render(
   <AuthProvider authType="cookie" authName="_auth" 
                 cookieDomain={window.location.hostname} cookieSecure={false}>
     <BrowserRouter>
-      <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
     </BrowserRouter>
   </AuthProvider>
 );
