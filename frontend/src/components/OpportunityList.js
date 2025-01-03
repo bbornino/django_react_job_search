d0bd4c488dacde168a597b69d4273093d200e334
Brian Bornino <brianbornino@gmail.com>
2025-01-03 09:40:28 -0800

    Refactor: OpportunityList useApiRequest

--- frontend/src/components/OpportunityList.js
+++ frontend/src/components/OpportunityList.js
@@ -1,30 +1,34 @@
-import {React, Component} from "react";
-import { Link } from 'react-router-dom'
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useApiRequest } from '../useApiRequest'; // import the hook
 import { JOB_OPPORTUNITY_API_URL, formatDisplayDateTime } from "../constants";
 
 import DataTableBase from './DataTableBase';
-import {Button, Container, Row, Col} from 'reactstrap';
+import { Button, Container, Row, Col } from 'reactstrap';
 
-class OpportunityList extends Component {
-    state = {
-        opportunities: [],
-    };
+const OpportunityList = () => {
+    const [opportunities, setOpportunities] = useState([]);
+    const { request, error } = useApiRequest(); // Using the custom hook
+    const navigate = useNavigate();
 
-    componentDidMount() {
+    useEffect(() => {
         document.title = "Opportunity List - Job Search Tracker";
-        this.resetState();
-    }
+        getOpportunities();
+    }, []);
 
-    resetState = () => {
-        this.getOpportunities();
-    };
-    
-    getOpportunities = () => {
-        axios.get(JOB_OPPORTUNITY_API_URL).then(res => this.setState({ opportunities: res.data }));
+    const getOpportunities = async () => {
+        const data = await request(
+            JOB_OPPORTUNITY_API_URL,
+            { method: 'GET' }
+        );
+        if (data) {
+            setOpportunities(data);
+        } else {
+            console.error('Failed to fetch opportunities');
+        }
     };
 
-    columns = [
+    const columns = [
         {
             name: 'Title',
             selector: row => row.job_title,
@@ -45,39 +49,38 @@ class OpportunityList extends Component {
             selector: row => row.email_received_at,
             cell: row => formatDisplayDateTime(row.email_received_at),
             sortable: true,
-            id:'email_received_at',
+            id: 'email_received_at',
             width: "250px",
         },
     ];
 
-    onRowClicked = (row, event) => { 
-        window.location = '/opportunity-details/' + row.id
+    const onRowClicked = (row, event) => {
+        navigate('/opportunity-details/' + row.id);
     };
 
-
-    render() {
-        const { opportunities } = this.state;
-        return (
-            <Container className="mt-2">
-                <Row className="m-4">
-                    <Col xxl="10" xl="9" lg="9" md="8" sm="5" xs="3">
-                        <h1>All Opportunities</h1>
-                    </Col>
-                    <Col xxl="2" xl="3" lg="3" md="4" sm="7" xs="9" className="pull-right">
-                        <Link to='/opportunity-details'>
-                            <Button>
-                                Create Opportunity
-                            </Button>
-                        </Link>
-                    </Col>
-                </Row>
-                <DataTableBase  columns={this.columns} data={opportunities} 
-                                defaultSortFieldId="email_received_at"
-                                defaultSortAsc={false}
-                                onRowClicked={this.onRowClicked} />
-            </Container>
-        )
-    }
-}
+    return (
+        <Container className="mt-2">
+            <Row className="m-4">
+                <Col xxl="10" xl="9" lg="9" md="8" sm="5" xs="3">
+                    <h1>All Opportunities</h1>
+                </Col>
+                <Col xxl="2" xl="3" lg="3" md="4" sm="7" xs="9" className="pull-right">
+                    <Link to='/opportunity-details'>
+                        <Button>
+                            Create Opportunity
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>
+            <DataTableBase 
+                columns={columns} 
+                data={opportunities} 
+                defaultSortFieldId="email_received_at"
+                defaultSortAsc={false}
+                onRowClicked={onRowClicked} 
+            />
+        </Container>
+    );
+};
 
 export default OpportunityList;
