c6b1d526e290fc308838194e83b02fa49c46ee19
Brian Bornino <brianbornino@gmail.com>
2025-01-05 10:19:22 -0800

    Refactor: Convert About to functional component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,51 +1,65 @@
-import React, { Component } from "react";
-import {Container, Row, Col} from 'reactstrap';
+import React, { useEffect, useRef } from "react";
+import { Container, Row, Col } from "reactstrap";
 
-class About extends Component {
-    componentDidMount() {
-        document.title = "About - Job Search Tracker";
-    }
+const About = () => {
+    const menuRef = useRef(null); // Ref to the dropdown menu
+    const activeElementRef = useRef(null); // Ref to track focused element
 
-    render() {
-        return (
-            <Container className="mt-4 p-t-10">
-                <Row className="mb-3 text-center">
-                    <h1>About this Application</h1>
-                </Row>
+    // Effect to manage focus when the component mounts/unmounts
+    useEffect(() => {
+        // Track the currently focused element
+        const activeElement = document.activeElement;
+        activeElementRef.current = activeElement;
 
-                <Row>
-                    <Col>
-                        <p>The job hunt is exhausting.  And it can be difficult to keep track of all of the different sites that
-                            you can find jobs on and all of the job listings that you have applied to.  Yes, the non-tech approach 
-                            is to create a spreadsheet for that.  But when you have job sites which contain postings which you may 
-                            have none to multiple comments...  you are then using normalized data which really lends 
-                            itself well to writing an app for that!
-                        </p>
-                        <p>Some of the high level features of this application include:</p>
+        // Copy the current value of menuRef to a variable for cleanup
+        const menuElement = menuRef.current;
+
+        // Cleanup to ensure focus is removed if necessary
+        return () => {
+            if (menuElement && menuElement.contains(activeElementRef.current)) {
+                activeElementRef.current.blur(); // Remove focus if it's on a hidden element
+            }
+        };
+    }, []); // Empty array to run on mount/unmount only
+
+    return (
+        <Container className="mt-4 p-t-10">
+            <Row className="mb-3 text-center">
+                <h1>About this Application</h1>
+            </Row>
+
+            <Row>
+                <Col>
+                    <p>
+                        The job hunt is exhausting...
+                    </p>
+                    <p>Some of the high-level features of this application include:</p>
+                    <ul>
+                        <li>Comments - Create, Read, Edit, and Delete</li>
+                        <li>Job Opportunity WYSIWYG Editor (CKEditor5)</li>
+                    </ul>
+                    <p>This application is built using the following technologies:</p>
+                    <ul>
+                        <li>Front End: React.js Single Page Application</li>
                         <ul>
-                            <li>Comments - Create, Read, Edit, and Delete</li>
-                            <li>Job Opportunity WYSIWYG Editor (CKEditor5)</li>
+                            <li>react-router-dom</li>
+                            <li>Bootstrap (using react-strap)</li>
+                            <li>react-data-table-component</li>
+                            <li>CK Editor 5</li>
+                            <li>Font Awesome</li>
+                            <li>React-auth-kit</li>
                         </ul>
-                        <p>
-                        This application is built using the following technologies:
-                        </p>
+                        <li>Python Django REST framework</li>
                         <ul>
-                            <li>Front End: React.js Single Page Application (using Router)</li>
-                                <ul>
-                                    <li>Bootstrap (using react-strap)</li>
-                                    <li>react-data-table-component</li>
-                                    <li>CK Editor 5</li>
-                                    <li>Font Awesome</li>
-                                </ul>
-                            <li>Python Django REST framework</li>
-                            <li>Database: sqlite, which can easily be migrated to MariaDB or AWS Aurora.  The eventual goal is to 
-                                migrate to MongoDB then AWS DynamoDB. I have discovered there is a hack for that... </li>
+                            <li>CORSheaders</li>
+                            <li>REST Framework Simple JWT (and Tokens)</li>
                         </ul>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-}
+                        <li>Database: sqlite, which can easily be migrated to MariaDB or AWS Aurora...</li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default About;
